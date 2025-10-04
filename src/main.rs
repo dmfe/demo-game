a4a3ff6eed7df498faee3791d7479231dcdb62e7
Dmitry Fedotov <dm.fe@yandex.ru>
2025-10-04 22:39:00 +0300

    Add text

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,25 @@ use macroquad::prelude::*;
 #[macroquad::main("Demo Game")]
 async fn main() {
     loop {
-        clear_background(DARKPURPLE);
+        clear_background(BLUE);
+
+        draw_text(
+            "CRITICAL ERROR DETECTED!!!",
+            200.0,
+            200.0,
+            50.0,
+            WHITE
+        );
+
+        let fps_text = format!("FPS: {}", get_fps());
+        draw_text(
+            &fps_text,
+            20.0,
+            20.0,
+            30.0,
+            RED
+        );
+
         next_frame().await
     }
 }
